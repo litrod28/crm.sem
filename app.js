9268b657064a0e0f9c6fa03f0ba878ad7c9cc8c9
litrod28 <28sushantkmr@gmail.com>
2025-08-11 15:16:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const SUPABASE_URL =
 const SUPABASE_ANON_KEY =
   window?.process?.env?.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
   window.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
-  'YOUR-ANON-KEY-HERE';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pYmh3c3h3cmdkeGZ4eWxrbHppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ2NzY5MjgsImV4cCI6MjA3MDI1MjkyOH0.c9Q9Q8dgC3EhveLV9iXeWpzka9xNlYctvGvwJN2T-og';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: { persistSession: false },
